74714d8b92efbbd13733963a4b3acbab43bea027
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-10-07 20:43:19 +0000

    SDK regeneration

--- src/Hume/EmpathicVoice/Chat/ChatApi.cs
+++ src/Hume/EmpathicVoice/Chat/ChatApi.cs
@@ -134,6 +134,9 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// Sets number of audio channels for audio input.
+    /// </summary>
     public int? SessionSettingsAudioChannels
     {
         get => ApiOptions.SessionSettingsAudioChannels;
@@ -144,6 +147,9 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// Sets encoding format of the audio input, such as `linear16`.
+    /// </summary>
     public string? SessionSettingsAudioEncoding
     {
         get => ApiOptions.SessionSettingsAudioEncoding;
@@ -154,6 +160,9 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// Sets the sample rate for audio input. (Number of samples per second in the audio input, measured in Hertz.)
+    /// </summary>
     public int? SessionSettingsAudioSampleRate
     {
         get => ApiOptions.SessionSettingsAudioSampleRate;
@@ -164,6 +173,11 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// The context to be injected into the conversation. Helps inform the LLM's response by providing relevant information about the ongoing conversation.
+    ///
+    /// This text will be appended to the end of [user_messages](/reference/speech-to-speech-evi/chat#receive.UserMessage.message.content) based on the chosen persistence level. For example, if you want to remind EVI of its role as a helpful weather assistant, the context you insert will be appended to the end of user messages as `{Context: You are a helpful weather assistant}`.
+    /// </summary>
     public string? SessionSettingsContextText
     {
         get => ApiOptions.SessionSettingsContextText;
@@ -174,6 +188,13 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// The persistence level of the injected context. Specifies how long the injected context will remain active in the session.
+    ///
+    /// - **Temporary**: Context that is only applied to the following assistant response.
+    ///
+    /// - **Persistent**: Context that is applied to all subsequent assistant responses for the remainder of the Chat.
+    /// </summary>
     public string? SessionSettingsContextType
     {
         get => ApiOptions.SessionSettingsContextType;
@@ -184,6 +205,9 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// Used to manage conversational state, correlate frontend and backend data, and persist conversations across EVI sessions.
+    /// </summary>
     public string? SessionSettingsCustomSessionId
     {
         get => ApiOptions.SessionSettingsCustomSessionId;
@@ -194,6 +218,9 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// The maximum number of chat events to return from chat history. By default, the system returns up to 300 events (100 events per page × 3 pages). Set this parameter to a smaller value to limit the number of events returned.
+    /// </summary>
     public int? SessionSettingsEventLimit
     {
         get => ApiOptions.SessionSettingsEventLimit;
@@ -204,6 +231,11 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// Third party API key for the supplemental language model.
+    ///
+    /// When provided, EVI will use this key instead of Hume's API key for the supplemental LLM. This allows you to bypass rate limits and utilize your own API key as needed.
+    /// </summary>
     public string? SessionSettingsLanguageModelApiKey
     {
         get => ApiOptions.SessionSettingsLanguageModelApiKey;
@@ -216,6 +248,15 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// Instructions used to shape EVI's behavior, responses, and style for the session.
+    ///
+    /// When included in a Session Settings message, the provided Prompt overrides the existing one specified in the EVI configuration. If no Prompt was defined in the configuration, this Prompt will be the one used for the session.
+    ///
+    /// You can use the Prompt to define a specific goal or role for EVI, specifying how it should act or what it should focus on during the conversation. For example, EVI can be instructed to act as a customer support representative, a fitness coach, or a travel advisor, each with its own set of behaviors and response styles.
+    ///
+    /// For help writing a system prompt, see our [Prompting Guide](/docs/speech-to-speech-evi/guides/prompting).
+    /// </summary>
     public string? SessionSettingsSystemPrompt
     {
         get => ApiOptions.SessionSettingsSystemPrompt;
@@ -226,6 +267,13 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// This field allows you to assign values to dynamic variables referenced in your system prompt.
+    ///
+    /// Each key represents the variable name, and the corresponding value is the specific content you wish to assign to that variable within the session. While the values for variables can be strings, numbers, or booleans, the value will ultimately be converted to a string when injected into your system prompt.
+    ///
+    /// Using this field, you can personalize responses based on session-specific details. For more guidance, see our [guide on using dynamic variables](/docs/speech-to-speech-evi/features/dynamic-variables).
+    /// </summary>
     public string? SessionSettingsVariables
     {
         get => ApiOptions.SessionSettingsVariables;
@@ -236,6 +284,9 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             );
     }
 
+    /// <summary>
+    /// The name or ID of the voice from the `Voice Library` to be used as the speaker for this EVI session. This will override the speaker set in the selected configuration.
+    /// </summary>
     public string? SessionSettingsVoiceId
     {
         get => ApiOptions.SessionSettingsVoiceId;
@@ -256,19 +307,6 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
             NotifyIfPropertyChanged(
                 EqualityComparer<string>.Default.Equals(ApiOptions.VerboseTranscription),
                 ApiOptions.VerboseTranscription = value
-            );
-    }
-
-    /// <summary>
-    /// The name or ID of the voice from the `Voice Library` to be used as the speaker for this EVI session. This will override the speaker set in the selected configuration.
-    /// </summary>
-    public string? VoiceId
-    {
-        get => ApiOptions.VoiceId;
-        set =>
-            NotifyIfPropertyChanged(
-                EqualityComparer<string>.Default.Equals(ApiOptions.VoiceId),
-                ApiOptions.VoiceId = value
             );
     }
 
@@ -310,7 +348,6 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
                 { "session_settings[variables]", SessionSettingsVariables },
                 { "session_settings[voice_id]", SessionSettingsVoiceId },
                 { "verbose_transcription", VerboseTranscription },
-                { "voice_id", VoiceId },
                 { "api_key", ApiKey },
             },
         }.Uri;
@@ -577,37 +614,83 @@ public partial class ChatApi : AsyncApi<ChatApi.Options>
         /// </summary>
         public string? ResumedChatGroupId { get; set; }
 
+        /// <summary>
+        /// Sets number of audio channels for audio input.
+        /// </summary>
         public int? SessionSettingsAudioChannels { get; set; }
 
+        /// <summary>
+        /// Sets encoding format of the audio input, such as `linear16`.
+        /// </summary>
         public string? SessionSettingsAudioEncoding { get; set; }
 
+        /// <summary>
+        /// Sets the sample rate for audio input. (Number of samples per second in the audio input, measured in Hertz.)
+        /// </summary>
         public int? SessionSettingsAudioSampleRate { get; set; }
 
+        /// <summary>
+        /// The context to be injected into the conversation. Helps inform the LLM's response by providing relevant information about the ongoing conversation.
+        ///
+        /// This text will be appended to the end of [user_messages](/reference/speech-to-speech-evi/chat#receive.UserMessage.message.content) based on the chosen persistence level. For example, if you want to remind EVI of its role as a helpful weather assistant, the context you insert will be appended to the end of user messages as `{Context: You are a helpful weather assistant}`.
+        /// </summary>
         public string? SessionSettingsContextText { get; set; }
 
+        /// <summary>
+        /// The persistence level of the injected context. Specifies how long the injected context will remain active in the session.
+        ///
+        /// - **Temporary**: Context that is only applied to the following assistant response.
+        ///
+        /// - **Persistent**: Context that is applied to all subsequent assistant responses for the remainder of the Chat.
+        /// </summary>
         public string? SessionSettingsContextType { get; set; }
 
+        /// <summary>
+        /// Used to manage conversational state, correlate frontend and backend data, and persist conversations across EVI sessions.
+        /// </summary>
         public string? SessionSettingsCustomSessionId { get; set; }
 
+        /// <summary>
+        /// The maximum number of chat events to return from chat history. By default, the system returns up to 300 events (100 events per page × 3 pages). Set this parameter to a smaller value to limit the number of events returned.
+        /// </summary>
         public int? SessionSettingsEventLimit { get; set; }
 
+        /// <summary>
+        /// Third party API key for the supplemental language model.
+        ///
+        /// When provided, EVI will use this key instead of Hume's API key for the supplemental LLM. This allows you to bypass rate limits and utilize your own API key as needed.
+        /// </summary>
         public string? SessionSettingsLanguageModelApiKey { get; set; }
 
+        /// <summary>
+        /// Instructions used to shape EVI's behavior, responses, and style for the session.
+        ///
+        /// When included in a Session Settings message, the provided Prompt overrides the existing one specified in the EVI configuration. If no Prompt was defined in the configuration, this Prompt will be the one used for the session.
+        ///
+        /// You can use the Prompt to define a specific goal or role for EVI, specifying how it should act or what it should focus on during the conversation. For example, EVI can be instructed to act as a customer support representative, a fitness coach, or a travel advisor, each with its own set of behaviors and response styles.
+        ///
+        /// For help writing a system prompt, see our [Prompting Guide](/docs/speech-to-speech-evi/guides/prompting).
+        /// </summary>
         public string? SessionSettingsSystemPrompt { get; set; }
 
+        /// <summary>
+        /// This field allows you to assign values to dynamic variables referenced in your system prompt.
+        ///
+        /// Each key represents the variable name, and the corresponding value is the specific content you wish to assign to that variable within the session. While the values for variables can be strings, numbers, or booleans, the value will ultimately be converted to a string when injected into your system prompt.
+        ///
+        /// Using this field, you can personalize responses based on session-specific details. For more guidance, see our [guide on using dynamic variables](/docs/speech-to-speech-evi/features/dynamic-variables).
+        /// </summary>
         public string? SessionSettingsVariables { get; set; }
 
+        /// <summary>
+        /// The name or ID of the voice from the `Voice Library` to be used as the speaker for this EVI session. This will override the speaker set in the selected configuration.
+        /// </summary>
         public string? SessionSettingsVoiceId { get; set; }
 
         /// <summary>
         /// A flag to enable verbose transcription. Set this query parameter to `true` to have unfinalized user transcripts be sent to the client as interim UserMessage messages. The [interim](/reference/speech-to-speech-evi/chat#receive.UserMessage.interim) field on a [UserMessage](/reference/speech-to-speech-evi/chat#receive.UserMessage) denotes whether the message is "interim" or "final."
         /// </summary>
         public bool? VerboseTranscription { get; set; }
-
-        /// <summary>
-        /// The name or ID of the voice from the `Voice Library` to be used as the speaker for this EVI session. This will override the speaker set in the selected configuration.
-        /// </summary>
-        public string? VoiceId { get; set; }
 
         /// <summary>
         /// API key used for authenticating the client. If not provided, an `access_token` must be provided to authenticate.
